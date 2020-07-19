eacabd969ea0af6a20dc93458074f5f434be4628
Tugrul Guner <47668043+tugrulguner@users.noreply.github.com>
2020-07-19 15:59:18 -0400

    Update imageloadandresize.py

--- imageloadandresize.py
+++ imageloadandresize.py
@@ -2,5 +2,9 @@ import cv2
 import numpy as np
 import glob
 
-def imreadandresffold(path, fileextension, sizeh, sizew):
-  imdata= np.stack([cv2.resize(cv2.imread(file), (sizeh,sizew), interpolation = cv2.INTER_NEAREST) for file in glob.glob(path+'/*.'+fileextension)])
+def imreadandresffold(path, fileextension, sizeh, sizew, operation):
+  if operation is 'all':
+    imdata= np.stack([cv2.resize(cv2.imread(file), (sizeh,sizew), interpolation = cv2.INTER_NEAREST) for file in glob.glob(path+'/*.'+ fileextension)])
+  if operation is 'specific':
+    imdata= np.stack([cv2.resize(cv2.imread(file), (sizeh,sizew), interpolation = cv2.INTER_NEAREST) for file in glob.glob(path+'/'+ specific + '.' + fileextension)])
+  return imdata
